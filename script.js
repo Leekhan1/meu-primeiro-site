7019269e0829df014e5cc252960c20ac87451a85
Leekhan1 <francisdielfraga@gmail.com>
2026-05-21 11:34:30 -0300

    Popup criar conta

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function entrarGuilda() {
   alert("Bem-vindo à Blood Oath ⚔️");
 
 }
+
+function abrirPopup() {
+
+  document.getElementById("popup").style.display = "flex";
+
+}
+
+function fecharPopup() {
+
+  document.getElementById("popup").style.display = "none";
+
+}
