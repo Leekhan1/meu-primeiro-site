dcae55a1d7e6e6804e62400d45f1bcf1244c5a6a
Leekhan1 <francisdielfraga@gmail.com>
2026-05-21 11:41:45 -0300

    Sistema login visual

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ function fecharPopup() {
   document.getElementById("popup").style.display = "none";
 
 }
+
+function login() {
+
+  let nome = document.getElementById("nomeJogador").value;
+
+  if (nome === "") {
+
+    alert("Digite o nome do personagem!");
+
+    return;
+
+  }
+
+  document.getElementById("login-section").style.display = "none";
+
+  document.getElementById("player-panel").style.display = "block";
+
+  document.getElementById("mensagemJogador").innerText =
+    "🔥 Bem-vindo, " + nome;
+
+}
